08891babdf8229a8dc938fcdbfc17d82f40beb39
ldizbarros <ldizbarros@victoria13.danielcastelao.org>
2018-01-11 09:18:34 +0100

    Inicio Proyecto

---
+++ src/cod_pullrequest/COD_PullRequest.java
@@ -0,0 +1,13 @@
+package cod_pullrequest;
+
+import javax.swing.JOptionPane;
+
+public class COD_PullRequest {
+
+    public static void main(String[] args) {
+        String saludo = JOptionPane.showInputDialog(null, 
+            "Hoooola!! Que tal??", 
+            JOptionPane.QUESTION_MESSAGE);
+    }
+    
+}
